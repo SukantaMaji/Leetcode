528ad3971688da9eb65c08cfee0b4a0641aabf41
Sukanta <sukantamaji527@gmail.com>
2025-08-11 19:01:31 +0530

    Time: 7 ms (55.18%) | Memory: 57 MB (40.29%) - LeetSync

---
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int[][] ans = new int[m][n];
+        // Build in method to fill the Array with any element(here -1).
+        for(int[] r: ans){
+            Arrays.fill(r, -1);
+        }
+        int minr = 0, maxr = m-1, minc = 0, maxc = n-1;
+        while(head!=null){
+            // print 1st row (Left->Right)
+            for(int j=minc; j<=maxc; j++){
+                if(head==null) break;
+                ans[minr][j] = head.val;
+                head = head.next;
+            } minr++;
+            // print last column (Up->Down)
+            for(int i=minr; i<=maxr; i++){
+                if(head==null) break;
+                ans[i][maxc] = head.val;
+                head = head.next;
+            } maxc--;
+            // print last row (Rigth->Left)
+            for(int j=maxc; j>=minc; j--){
+                if(head==null) break;
+                ans[maxr][j] = head.val;
+                head = head.next;
+            } maxr--;
+            // print 1st column (Down->Up)
+            for(int i=maxr; i>=minr; i--){
+                if(head==null) break;
+                ans[i][minc] = head.val;
+                head = head.next;
+            } minc++;
+            
+        }
+        return ans;
+        
+    }
+}
