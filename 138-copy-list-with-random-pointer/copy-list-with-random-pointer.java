55abb958daae6e60d1beb407bde46ec101b7b88b
Sukanta <sukantamaji527@gmail.com>
2025-09-04 14:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (11.86%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -1,18 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
 class Solution {
     public Node deepCopy(Node head){
         if(head == null) return null;
