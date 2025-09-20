55fcaad8cc45e058e4730895472afa51facd7a3d
Sukanta <sukantamaji527@gmail.com>
2025-09-20 13:34:18 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (84.00%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        for(int i=1; i<=s.length(); i++){
-            // Built in rotate method(starting, Where to rotate, ending)
-            // If says rotate k places then rotate(begin(s), begin(s)+k, end(s))
-            rotate(begin(s), begin(s)+1, end(s));
-            if(s == goal) return true;
-        }
+        if(s.length() != goal.length()) return false;
+
+        /* If we concatenate a string with itself, then it will
+         contain all rotations as a substring.
+        */
+
+        if((s+s).find(goal) != string::npos) return true;
         return false;
     }
 };
